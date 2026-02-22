d03380c7fbb28ad55c795ca0f4dc482278923310
EricB <ebo12@gcloud.ua.es>
2026-02-22 20:24:23 +0100

    LPP Practica 4 acabada

--- Segundo Curso/LPP/practica4.rkt
+++ Segundo Curso/LPP/practica4.rkt
@@ -1,1 +1,18 @@
 #lang racket
+
+;Ejericio 1
+;########################
+          ;a)
+;########################
+(define (es-prefijo? prefijo palabra)
+  (equal? (substring palabra 0 (string-length prefijo)) prefijo))
+
+(define (contiene-prefijo prefijo lista-pal)
+  (if (null? lista-pal)
+      '()
+  (cons (es-prefijo? prefijo (first lista-pal))
+        (contiene-prefijo prefijo (rest lista-pal)))))
+
+;########################
+          ;b)
+;########################
