f025334dc7b78ee724f9a4e67ad448ff174f35e7
EricB <ebo12@gcloud.ua.es>
2026-03-01 12:20:18 +0100

    LPP

--- Segundo Curso/LPP/practica4.rkt
+++ Segundo Curso/LPP/practica4.rkt
@@ -16,3 +16,100 @@
 ;########################
           ;b)
 ;########################
+
+(define (mayor pal1 pal2)
+  (> (string-length pal1) (string-length pal2)))
+
+
+(define (cadenas-mayores lista1 lista2)
+  (cond [(null? lista1)
+        lista2]
+      [(null? lista2)
+        lista1]
+        [(mayor (first lista1) (first lista2))
+        (cons (first lista1) (cadenas-mayores (rest lista1) (rest lista2)))]
+        [(mayor (first lista2) (first lista1))
+        (cons (first lista2) (cadenas-mayores (rest lista1) (rest lista2)))]))
+
+
+;Ejercicio 2
+;########################
+          ;a)
+;########################
+
+
+(define (inserta-pos dato pos lista)
+  (cond [(null? lista)
+         '()]
+        [(= pos 0)
+         (cons dato lista)]
+        [else
+         (cons (first lista) (inserta-pos dato (- pos 1) (rest lista)))]))
+
+;########################
+          ;b)
+;########################
+
+(define (inserta-ordenada n lista-ordenada)
+  (cond [(null? lista-ordenada)
+         lista-ordenada n]
+        [(< n (first lista-ordenada))
+         (cons n lista-ordenada)]
+        [else
+         (cons (first lista-ordenada) (inserta-ordenada n (rest lista-ordenada)))]))
+
+(define (menor-lista lista)
+  (cond [(null? (rest lista))
+         (first lista)]
+        [(< (first lista) (second lista))
+         (menor-lista (cons (first lista) (rest (rest lista))))]
+        [else
+         (menor-lista (rest lista))]))
+
+(define (borra-numero n lista)
+  (cond [(null? lista) '()]
+        [(= n (first lista)) (rest lista)]
+        [else (cons (first lista) (borra-numero n (rest lista)))]))
+
+(define (ordena lista-des)
+  (cond [(null? lista-des)
+         '()]
+        [else
+         (cons (menor-lista lista-des) 
+               (ordena (borra-numero (menor-lista lista-des) lista-des)))]))
+         
+
+       
+             
+      
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
